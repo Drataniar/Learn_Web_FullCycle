13d256a28f95e178f5bbc9bb9f6e79f78632286d
Drataniar <83446717+Drataniar@users.noreply.github.com>
2024-12-03 18:14:23 +0900

    6주차 02 - user.js 리펙토링

---
+++ Youtube-demo/mariadb.js
@@ -0,0 +1,15 @@
+// Get the client
+const mysql = require('mysql2');
+
+// Create the connection to database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password : 'root',
+  database: 'YouTube',
+  dateStrings : true,
+});
+
+
+
+module.exports = connection;
--- Youtube-demo/routes/users.js
+++ Youtube-demo/routes/users.js
@@ -1,144 +1,127 @@
 const express = require('express');
-//const app = express();
-//app.listen(3000);
 const router = express.Router()
-
-var db = new Map();
+const connection = require('../mariadb');
 
 router.use(express.json())
 
 router.post('/login',function(req,res){
     let loginInfo = req.body;
-    console.log(loginInfo.userId);
-    console.log(loginInfo.password);
     
-
-    if(loginInfo.userId && loginInfo.password)
+    if(loginInfo.userEmail && loginInfo.password)
     {   
-        let userInfo = db.get(loginInfo.userId);
-
-        if(userInfo)
-        {
-            if(userInfo.password === loginInfo.password)
-            {
-                let userName = db.get(loginInfo.userId).name;
-
-                res.status(201).json({
-                    message : `${userName}님 환영합니다.`
-                })
+        let sql = `SELECT * FROM users WHERE email = ?`;
+        connection.query(sql,loginInfo.userEmail,function (err, results) {
+                const userInfo = results[0];
+                if(userInfo && userInfo.password === loginInfo.password)
+                    {
+                            res.status(201).json({
+                                message : `${userInfo.name}님 환영합니다.`
+                            })
+                    }
+                    else {
+                        res.status(400).json({
+                            message : "아이디 혹은 비밀번호가 틀렸습니다"
+                        });
+                    }
             }
-            else{
-                res.status(400).json({
-                    message : "아이디 혹은 비밀번호가 틀렸습니다"
-                });
-            }
-        }
-        else{
-            res.status(400).json({
-                message : "아이디 혹은 비밀번호가 틀렸습니다"
-            });
-        }
-        
+          );
+    }
+    else if(!loginInfo.userEmail){
+        res.status(400).json({
+            message : "아이디를 입력해 주십시요"
+        });
     }
     else{
         res.status(400).json({
-            message : "아이디와 비밀번호를 제대로 입력해 주십시요"
+            message : "비밀번호를 입력해 주십시요"
         });
     }
 })
 
 router.post('/join',function(req,res){
-    let loginInfo = req.body;
-    //console.log(loginInfo);
-    //console.log(loginInfo.id);
-    let userId = loginInfo.userId;
-    if(userId)
+    let userInfoInput = req.body;
+    let userEmail = userInfoInput.email;
+    if(userEmail)
     {
-        if(!db.get(userId))
-        {
-            if(loginInfo.name && loginInfo.password)
-                {
-                    db.set(loginInfo.userId,loginInfo);
-            
-                    res.json({
-                            message : `${db.get(loginInfo.userId).name}님 환영합니다.`
+        const { name, password, contact} = req.body;
+        let sql = `INSERT INTO users (email, name, password, contact) VALUE(?,?,?,?)`;
+        let inputs = [userEmail, name, password, contact];
+        connection.query(sql,inputs,function (err, results) {
+                var userInfo = results[0];
+                if(userInfo)
+                    {
+                        if(name && password)
+                            {
+                            
+                                res.status(201).json({
+                                    message : `${userInfo.name}님 환영합니다.`
+                                });
+                            }
+                            else{
+                                res.status(400).json({
+                                    message : "회원정보를 제대로 입력해 주십시요"
+                                });
+                            }
+                    }
+                    else{
+                        res.status(400).json({
+                            message : "중복되는 아이디입니다. 다른 아이디로 입력해주세요"
                         });
-                }
-                else{
-                    res.status(400).json({
-                        message : "회원정보를 제대로 입력해 주십시요"
-                    });
-                }
-        }
-        else{
-            res.status(400).json({
-                message : "중복되는 아이디입니다. 다른 아이디로 입력해주세요"
-            });
-        }
+                    }
+            }
+          );
+        
     }
     else{
         res.status(400).json({
             message : "아이디를 입력해 주십시요"
         });
     }
-    
-    
 })
 
 router.route('/users')
     .get(function(req,res){
-        let id = req.body.userId;
-        let userInfo = db.get(id);
-        if(userInfo)
-        {
-            res.json({
-                userId : userInfo.userId,
-                name : userInfo.name
-            });
-        }
-        else{
-            res.status(404).json({
-                message : "아이디에 해당하는 정보가 없습니다."
-            });
-        }
-        
+        let email = req.body.userEmail;
+
+        let sql = `SELECT * FROM users WHERE email = ?`;
+        connection.query(sql,email,function (err, results) {
+                if(results.length){
+                    res.json(results);
+                }
+                else{
+                    res.status(404).json({
+                        message : "아이디에 해당하는 정보가 없습니다."
+                    });
+                }
+            }
+          );
     })
     .delete(function(req,res){
-        let id = req.body.userId;
-        let deletedUser = db.get(id);
-
-        if(deletedUser)
-        {
-            db.delete(id);
-            res.json({
-                message : `${deletedUser.name}님 다음에 안녕히가십시오`
-            })
-        }
-        else{
-            res.status(404).json({
-                message : "회원 정보를 찾을 수 없습니다."
-            })
-        }
-        
+        let emailInput = req.body.userEmail;
+        var deletedUser;
+        let sql = `SELECT * FROM users WHERE email = ?`;
+        connection.query(sql,emailInput,function (err, results) {
+                deletedUser = results[0];
+                if(deletedUser)
+                    {
+                        const { email} = req.body;
+                        let deleteSql = `DELETE FROM users WHERE email = ?`;
+                            connection.query(deleteSql,email,function (err, results) {
+                                    res.json({
+                                        message : `${deletedUser.name}님 다음에 안녕히가십시오`
+                                    })
+                                    }
+                                  );
+                    }
+                    else{
+                        res.status(404).json({
+                            message : "회원 정보를 찾을 수 없습니다."
+                        })
+                    }
+                
+            });
     })
 
-    function isEmpty(obj){
-        if(Object.keys(obj).length === 0)
-        {
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
 
-    function searchUserbyId(id){
-        db.forEach(function(user,temp){
-            if(user.id === id){
-                return user;
-            }
-        })
-        return undefined;
-    }
 
     module.exports = router;
